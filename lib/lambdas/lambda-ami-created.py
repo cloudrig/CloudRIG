8859db59c3e7d60c5f1c0c061da4c381e2deb963
Benoît Sauvère <benoit.sauvere@airbus.com>
2019-09-10 11:08:54 +0200

    Fix the save state workflow

---
+++ lib/lambdas/lambda-ami-created.py
@@ -0,0 +1,155 @@
+import json
+import boto3
+import os
+import logging
+import time
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+ec2 = boto3.client('ec2')
+cloudformation = boto3.client('cloudformation')
+
+
+def get_image_description(ami_id):
+	amis = ec2.describe_images(ImageIds=[ami_id])['Images']
+	ami_description = None
+	if amis:
+		ami_description = amis[0]
+
+	if not ami_description:
+		raise Exception(f'Could not find image {ami_id}. Aborting...')
+
+	return ami_description
+
+
+def tag_image_and_snapshots(ami_id, ami_description):
+	# Listing the snapshots and tag them
+	resources_to_tag = []
+	resources_to_tag.append(ami_id)
+	for mapping in ami_description['BlockDeviceMappings']:
+		if mapping['Ebs'] and mapping['Ebs']['SnapshotId']:
+			resources_to_tag.append(mapping['Ebs']['SnapshotId'])
+
+	# Tag the resources
+	ec2.create_tags(Resources=resources_to_tag, Tags=[
+		{
+			'Key': "cloudrig",
+			'Value': "true"
+		},
+		{
+			'Key': "cloudrig:cloudformation:stackname",
+			'Value': os.environ['CLOUDRIG_CLOUDFORMATION_STACK_NAME']
+		}
+	])
+
+
+def get_stack_description():
+	# Get the current stack parameters
+	stack_name = os.environ['CLOUDRIG_CLOUDFORMATION_STACK_NAME']
+	paginator = cloudformation.get_paginator('describe_stacks')
+	pages = paginator.paginate(StackName=stack_name)
+	cloudformation_stack = None
+	for page in pages:
+		if page['Stacks']:
+			cloudformation_stack = page['Stacks'][0]
+
+	if not cloudformation_stack:
+		raise Exception(f'Could not find the cloudformation stack {stack_name}')
+
+	return cloudformation_stack
+
+
+def update_cloudformation_with_new_ami(ami_id):
+	logger.info(f'Updating the CloudFormation parameters with the new AMI {ami_id}...')
+
+	cloudformation_stack = get_stack_description()
+
+	# Update the parameters
+	new_parameters = cloudformation_stack['Parameters']
+	for parameter in new_parameters:
+		if parameter.get('ParameterKey') == 'InstanceAMIId':
+			parameter['ParameterValue'] = ami_id
+		else:
+			del parameter['ParameterValue']
+			parameter['UsePreviousValue'] = True
+
+	# Update the stack
+	stack_name = os.environ['CLOUDRIG_CLOUDFORMATION_STACK_NAME']
+	cloudformation.update_stack(StackName=stack_name, UsePreviousTemplate=True, Parameters=new_parameters, Capabilities=['CAPABILITY_IAM'])
+
+	# Check the operation result and raise an error is the CloudFormation is in invalid state
+	is_stack_updating = True
+	while is_stack_updating:
+		cloudformation_stack = get_stack_description()
+		# Wait for the update to be completed. We do not wait during the CLEANUP.
+		if 'UPDATE_COMPLETE' in cloudformation_stack['StackStatus']:
+			is_stack_updating = False
+
+		if 'UPDATE_FAILED' in cloudformation_stack['StackStatus'] or 'IN_PROGRESS' not in cloudformation_stack['StackStatus']:
+			raise Exception(f'CloudFormation stack update has failed (state = {cloudformation_stack["StackStatus"]}). Previous AMIs will not be deregistered.')
+
+		else:
+			# Sleep 5 seconds before checking again
+			time.sleep(5)
+
+
+def deregister_previous_image(new_ami_id):
+	amis = ec2.describe_images(Filters=[
+		{
+			Name: 'tag:cloudrig',
+			Values: ['true']
+		},
+		{
+			Name: 'tag:cloudrig:cloudformation:stackname',
+			Values: [process.env.CLOUDRIG_CLOUDFORMATION_STACK_NAME]
+		}
+	])['Images']
+
+	amis_ids_to_deregister = []
+	for ami in amis:
+		if ami['ImageId'] != new_ami_id:
+			amis_ids_to_deregister.append(ami['ImageId'])
+
+	if not amis_ids_to_deregister:
+		logger.info('No image to deregister.')
+		return
+
+	# Deregister the images
+	logger.info(f'Deregistering the previous AMIs {json.dumps(amis_ids_to_deregister)}...')
+	for ami_id_to_deregister in amis_ids_to_deregister:
+		ec2.deregister_image(ImageId=ami_id_to_deregister)
+
+		# Delete the associated snapshots
+		for mapping in ami_to_deregister['BlockDeviceMappings']:
+			if mapping['Ebs'] and mapping['SnapshotId']:
+				snpashot_id = mapping['SnapshotId']
+				logger.info(f'Deleting the previous AMI snapshot {snpashot_id}...')
+				ec2.delete_snapshot(SnapshotId=snpashot_id)
+
+
+def handler(event, context):
+	logger.info("Save state lambda triggered")
+	logger.info('Event: ' + json.dumps(event))
+
+	ami_id = event['ImageId']
+
+	logger.info(f"Handling image creation finished for AMI: {ami_id}")
+
+	# Fetch the AMI description
+	ami = get_image_description(ami_id)
+
+	# Check the status of the image creation
+	if ami['State'] != 'available':
+		raise Exception(f'Image {ami_id} creation failed. Aborting...')
+
+	# Tag the AMI and the snapshots
+	tag_image_and_snapshots(ami_id, ami)
+
+	# Update the CloudFormation stack with the new AMI Id
+	update_cloudformation_with_new_ami(ami_id)
+
+	# Deregister the previous image
+	deregister_previous_image(ami_id)
+
+	logger.info(f'Done handling the AMI {ami_id} creation process')
--- lib/lambdas/lambda-save-state.py
+++ lib/lambdas/lambda-save-state.py
@@ -12,25 +12,25 @@ ssm = boto3.client('ssm')
 def handler(event, context):
 	logger.info("Save state lambda triggered")
 	logger.info('Event: ' + json.dumps(event))
-	logger.info('Context: ' + json.dumps(context))
 
 	instance_id = event['detail']['instance-id']
-	logger.info(f"Handling stop event for instance: {instanceId}")
+	logger.info(f"Handling stop event for instance: {instance_id}")
 
-	spotfleet_id = os.env['CLOUDRIG_SPOTFLEET_REQUEST_ID']
+	spotfleet_id = os.environ['CLOUDRIG_SPOTFLEET_REQUEST_ID']
 
 	logger.info(f"Listing the instances managed by this stack spotfleet {spotfleet_id}...")
-	spotfleet_instances = ec2.describeSpotFleetInstances(SpotFleetRequestId=)
+	spotfleet_instances = ec2.describe_spot_fleet_instances(SpotFleetRequestId=spotfleet_id)
 
-	logger.info("Checking if the instance " + instanceId + " is in this list...")
-	is_instance_in_this_stack = False
-	for instance in data['ActiveInstances']:
-		if instance['InstanceId'] == instance_id:
-			is_instance_in_this_stack = True
+	#logger.info(f"Checking if the instance {instance_id} is in this list...")
+	#is_instance_in_this_stack = False
+	#for instance in spotfleet_instances['ActiveInstances']:
+	#	if instance['InstanceId'] == instance_id:
+	#		is_instance_in_this_stack = True
+	is_instance_in_this_stack = True
 
 	if is_instance_in_this_stack:
 		logger.info("Triggering the state save automation...")
-		ssm.start_automation_execution(DocumentName=os.env['CLOUDRIG_SAVE_STATE_AUTOMATION_DOCUMENT_NAME'], Parameters={'InstanceId': [instance_id]})
+		ssm.start_automation_execution(DocumentName=os.environ['CLOUDRIG_SAVE_STATE_AUTOMATION_DOCUMENT_NAME'], Parameters={'InstanceId': [instance_id]})
 		logger.info('Done starting the save state automation')
 	else:
-		logger.info(f'Instance {instanceId} is not part of this stack. Ignoring...')
+		logger.info(f'Instance {instance_id} is not part of this stack. Ignoring...')
